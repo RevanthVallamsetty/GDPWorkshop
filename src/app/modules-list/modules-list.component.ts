bf458cbfeb899113ce778bd40a8cb48adb746296
RevanthVallamsetty <33847680+RevanthVallamsetty@users.noreply.github.com>
2018-11-14 21:19:43 -0600

    worked on modules list

--- src/app/modules-list/modules-list.component.ts
+++ src/app/modules-list/modules-list.component.ts
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modules-list.component.scss']
 })
 export class ModulesListComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
+  }
+ 
+ 
+ detail(modulenum:number){
+  
+    console.log(modulenum)
+   var btns = document.getElementsByName("modules")
+   console.log(btns)
+   btns.forEach(function(btn){
+     if(btn.classList.contains("isEnabled")){
+      btn.classList.remove("isEnabled");
+      btn.classList.add("isDisabled");
+     }
+   })
+    switch(modulenum){
+      case 1:
+       document.getElementById("mod1").classList.remove("isDisabled");
+       document.getElementById("mod1").classList.add("isEnabled");
+       break
+      case 2:
+       document.getElementById("mod2").classList.remove("isDisabled");
+       document.getElementById("mod2").classList.add("isEnabled");
+       break
+      case 3:
+       document.getElementById("mod3").classList.remove("isDisabled");
+       document.getElementById("mod3").classList.add("isEnabled");
+       break
+      case 4:
+      document.getElementById("mod4").classList.remove("isDisabled");
+      document.getElementById("mod4").classList.add("isEnabled");
+      break
+      case 5:
+       document.getElementById("mod5").classList.remove("isDisabled");
+       document.getElementById("mod5").classList.add("isEnabled");
+       break
+       case 6:
+       document.getElementById("mod6").classList.remove("isDisabled");
+       document.getElementById("mod6").classList.add("isEnabled");
+       break
+       case 7:
+       document.getElementById("mod7").classList.remove("isDisabled");
+       document.getElementById("mod7").classList.add("isEnabled");
+       break
+    }
   }
 
 }

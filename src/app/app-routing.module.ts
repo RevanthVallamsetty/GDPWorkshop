b26603cbf5c0249ee9ba36b13bfe185ba68b4be3
RevanthVallamsetty <33847680+RevanthVallamsetty@users.noreply.github.com>
2018-11-15 21:30:47 -0600

    added routing and completed about and syllabus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SyllabusComponent } from './syllabus/syllabus.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { AboutTheCourseComponent } from './about-the-course/about-the-course.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'syllabus', component: SyllabusComponent},
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'about', component: AboutTheCourseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
